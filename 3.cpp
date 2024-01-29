e381c851d54dc67b7e46a7285a52c82e58de5a84
hetgondaliya <132036737+hetgondaliya40@users.noreply.github.com>
2024-01-29 17:26:41 +0530

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -78,213 +78,145 @@ a
 
 */
 #include <iostream>
-
 using namespace std;
 
-
-
-int main (){
-	
-	cout << endl << endl << endl << endl << endl << "\t\t\t\t\t\t -: BANK MANAGMENT SYSTEM  :- " ;
-	cout << endl  << "\t\t\t\t\t\t -----------------------------" ;
-	cout << endl << "\t\t\t\t\t\t -: Designed N Programed By :- "  ;
-	cout << endl << "\t\t\t\t\t\t -----------------------------" ;
-	cout << endl << "\t\t\t\t\t\t        HET R GONDALIYA  "  << endl ;
-	cout << endl << "\t\t\t\t\t\t *****************************" << endl ;
-	cout << endl << "\t\t\t\t\t\t\t -: Trainer :- " ;
-	cout << endl << "\t\t\t\t\t\t -----------------------------" ;
-	cout << endl << "\t\t\t\t\t\t       Gaurav Kanani     " << endl << endl << endl ;
-		
-	char start;
-	cout << "\t\t\t\t\t\tPess any key & enter to continue...";
-	cin >> start;
-
-	char log_in;
-	cout << endl << endl << endl << endl << endl << endl  << "\t\t\t\t\t ---------------------------------------------------- " ;
-	cout << endl << "\t\t\t\t\t -: Press A for login as administator S for staff :- "  ;
-	cout <<  endl << "\t\t\t\t\t ---------------------------------------------------- " << endl << endl << endl ;
-	cin  >> log_in;
-	
-	char name[100];
-	char email[100];
-	int phone_num;
-	int account_num;
-	int choice;
-	
-	if(log_in=='a' || log_in=='A'){
-	
-		cout << endl << endl << endl <<  "----------------------------------------------------------------------------------------------------------------------";
-		cout << endl << "\t\t\t\t\t\t -: Wellcome As administator :- ";
-		cout <<  endl <<  "----------------------------------------------------------------------------------------------------------------------"<<endl<<endl<<endl<<endl;\
-		cout << "\t\t\t\t   Enter The Name OF administator\t\t     : ";
-		cin >> name;
-		cout << "\t\t\t\t   Enter The Account NUmber Of administator          : ";
-		cin >> account_num;
-		cout << "\t\t\t\t   Enter The Phone NUmber Of administator\t     : ";
-		cin >> phone_num;
-		cout << "\t\t\t\t   Enter The E-mail Of administator\t             : ";
-		cin >> email;
-		cout << endl << endl << endl;
-
-		cout << endl  <<  "----------------------------------------------------------------------------------------------------------------------" << endl;
-		cout << "   Name           :- "<< name << endl;
-		cout << "   Account NUmber :- " << account_num << endl;
-		cout << "   Phone NUmber   :- " << phone_num << endl;
-		cout << "   E-mail         :- " << email ;
-		cout <<  endl <<  "----------------------------------------------------------------------------------------------------------------------"<<endl<<endl<<endl;
-		cout << "\t\t\t\t\t-: Press [1] to Deposite the money :-" << endl << endl ;
-		cout << "\t\t\t\t\t-: Press [2] to Transfer the money :-" << endl << endl;
-		cout << "\t\t\t\t\t-: Press [3] to Withdraw the money :-" << endl << endl;
-		cin >> choice;
-		
-		
-		switch(choice){
-			
-			case 1:
-		
-				int deposite,after_bal;
-				cout << endl  <<  "----------------------------------------------------------------------------------------------------------------------" << endl;
-				cout << "   Enter Deposite Amaout : ";
-				cin >> deposite;
-				cout <<  "----------------------------------------------------------------------------------------------------------------------"<<endl;
-				cout << endl << endl << endl;
-				cout << "\t\t\tYour Actual Amount Is : 200000" << endl << endl;
-				cout << "\t\t\tCongrates Your Amount Has deposite Successfully." << endl << endl;
-				after_bal=200000+deposite;
-				cout << "\t\t\tYour Account Balance Is : " << after_bal << endl << endl;
-			break;
-			
-			case 2:
-		
-				int transfer,after_bal1,dumy_account;
-				cout << endl  <<  "----------------------------------------------------------------------------------------------------------------------" << endl;
-				cout << "   Enter Amaout Which You Wanna Transfer : ";
-				cin >> transfer;
-				cout << "   Enter Another Account NUmber (Where You Wanna Transfer MOney ) : ";
-				cin >> dumy_account;
-				cout <<  "----------------------------------------------------------------------------------------------------------------------"<<endl;
-				cout << endl << endl << endl;
-				if(dumy_account==account_num){
-					cout << "This is your account number " << dumy_account;
-				}
-				else{
-					cout << "\t\t\tYour Actual Amount Is : 200000" << endl << endl;
-					cout << "\t\t\tCongrates Your Amount Has Transfer Successfully." << endl << endl;
-					after_bal1=200000-transfer;
-					cout << "\t\t\tYour Account Balance Is : " << after_bal1 << endl << endl;		
-			}
-			break;
-			
-			case 3:
-		
-				int withdraw,after_bal2;
-				cout << endl  <<  "----------------------------------------------------------------------------------------------------------------------" << endl;
-				cout << "   Enter Whithdraw Amaout : ";
-				cin >> withdraw;
-				cout <<  "----------------------------------------------------------------------------------------------------------------------"<<endl;
-				cout << endl << endl << endl;
-				cout << "\t\t\tYour Actual Amount Is : 200000" << endl << endl;
-				cout << "\t\t\tCongrates Your Amount Has Withdraw Successfully." << endl << endl;
-				after_bal2=200000-withdraw;
-				cout << "\t\t\tYour Account Balance Is : " << after_bal2 << endl << endl;
-				break;
-			
-			default:
-				cout << "Invalid choice.....";
-			break;				
+class Bank {
+    private:
+    	char pa1, pa2 ;
+    	int id_1,phone ;
+    	string name,email;
+        float balance=0;
+    public:
+    	
+    	Banner(){
+			cout<< "\t\t:-YOUR BANK MANGEMENT SYSTEM -: "<< endl;
+			cout<<"************************************************************ "<< endl ;
+			cout<<endl;
+			cout<< "\t\t:- Design And Program by -: "<< endl;
+			cout<<"************************************************************ "<< endl;
+			cout<<"\t\thet gondaliya" <<endl;
+			cout<<endl;
+			cout<< "\t\t        :- Trainer -:       "<< endl;
+			cout<<"************************************************************ "<< endl;
+			cout<<"\t\t\t gaurav sir         "<<endl;
+			cout<<endl;
+			cout<<endl;
+			cout<< "press any key to go to next page : "; cin>>pa1;
+			system("cls");
 		}
 		
-		
-	}
-	else if(log_in=='s' || log_in=='S'){
-	
-		cout << endl << endl << endl <<  "----------------------------------------------------------------------------------------------------------------------";
-		cout << endl << "\t\t\t\t\t\t -: Wellcome As staf :- ";
-		cout <<  endl <<  "----------------------------------------------------------------------------------------------------------------------"<<endl<<endl<<endl<<endl;\
-		cout << "\t\t\t\t\tEnter The Name OF Staff\t\t     : ";
-		cin >> name;
-		cout << "\t\t\t\t\tEnter The Account NUmber Of Staff    : ";
-		cin >> account_num;
-		cout << "\t\t\t\t\tEnter The Phone NUmber Of Staff\t     : ";
-		cin >> phone_num;
-		cout << "\t\t\t\t\tEnter The E-mail Of Satff\t     : ";
-		cin >> email;
-		cout << endl << endl << endl;
-
-		cout << endl  <<  "----------------------------------------------------------------------------------------------------------------------" << endl;
-		cout << "   Name           :- "<< name << endl;
-		cout << "   Account NUmber :- " << account_num << endl;
-		cout << "   Phone NUmber   :- " << phone_num << endl;
-		cout << "   E-mail         :- " << email ;
-		cout <<  endl <<  "----------------------------------------------------------------------------------------------------------------------"<<endl<<endl<<endl;
-		cout << "\t\t\t\t\t-: Press [1] to Deposite the money :-" << endl << endl ;
-		cout << "\t\t\t\t\t-: Press [2] to Transfer the money :-" << endl << endl;
-		cout << "\t\t\t\t\t-: Press [3] to Withdraw the money :-" << endl << endl;
-		cin >> choice;
-		
-		
-		switch(choice){
+		Staff(){
+			rep : 
+			cout<<"************************************************************** "<< endl;
+			cout<<" -: Press A to login as administrator or B to login as staff:-"<<endl;
+			cout<<"************************************************************** "<< endl;
+			cout<<endl;
+			cout<< "press a key to go to next page : "; cin>>pa2;
 			
-			case 1:
-		
-				int deposite,after_bal;
-				cout << endl  <<  "----------------------------------------------------------------------------------------------------------------------" << endl;
-				cout << "   Enter Deposite Amaout : ";
-				cin >> deposite;
-				cout <<  "----------------------------------------------------------------------------------------------------------------------"<<endl;
-				cout << endl << endl << endl;
-				cout << "\t\t\tYour Actual Amount Is : 200000" << endl << endl;
-				cout << "\t\t\tCongrates Your Amount Has deposite Successfully." << endl << endl;
-				after_bal=200000+deposite;
-				cout << "\t\t\tYour Account Balance Is : " << after_bal << endl << endl;
-			break;
-			
-			case 2:
-		
-				int transfer,after_bal1,dumy_account;
-				cout << endl  <<  "----------------------------------------------------------------------------------------------------------------------" << endl;
-				cout << "   Enter Amaout Which You Wanna Transfer : ";
-				cin >> transfer;
-				cout << "   Enter Another Account NUmber (Where You Wanna Transfer MOney ) : ";
-				cin >> dumy_account;
-				cout <<  "----------------------------------------------------------------------------------------------------------------------"<<endl;
-				cout << endl << endl << endl;
-				if(dumy_account==account_num){
-					cout << "This is your account number " << dumy_account;
-				}
-				else{
-					cout << "\t\t\tYour Actual Amount Is : 200000" << endl << endl;
-					cout << "\t\t\tCongrates Your Amount Has Transfer Successfully." << endl << endl;
-					after_bal1=200000-transfer;
-					cout << "\t\t\tYour Account Balance Is : " << after_bal1 << endl << endl;		
+			if(pick=='A' || pick=='a')
+			{
+				cout<<"\n************************************************************" ;
+				cout<<"\nWelcome to administration page : ";
+				cout<<"\n************************************************************" ;
+				cout<< endl;
+				cout<< "\tEnter The Name Of Bank Staff Member :" ; cin>>name ;
+				cout<< "\tEnter The ID of Bank Staff Member :" ; cin>>id_1 ;
+				cout<< "\tEnter The Phone Number Of Bank Staff Member :" ; cin>>phone ;
+				cout<< "\tEnter The Email Address of Bank Staff Member :"; cin>>email ;
+				
+				cout<<"\nSuccessfully Registered! : \n";
+				cout<<"***************************************************************"<<endl ;
+				cout<< "\n\t Name \t\t:" <<name;
+				cout<< "\n\t Amount Number\t:"<<id_1;
+				cout<< "\n\t Phone Number \t:"<<phone;
+				cout<< "\n\t Email \t\t:"<<email;
+				cout<<"\n*************************************************************"<<endl ;
 			}
-			break;
 			
-			case 3:
+			else if(pick=='B' || pick=='b')
+			{
+				cout<<"\n************************************************************" ;
+				cout<<"\nWelcome to staff panel : ";
+				cout<<"\n************************************************************" ;
+				cout<< endl;
+				cout<< "\tEnter The Name Of Bank Staff Member :" ; cin>>name ;
+				cout<< "\tEnter The ID of Bank Staff Member :" ; cin>>id_1 ;
+				cout<< "\tEnter The Phone Number Of Bank Staff Member :" ; cin>>phone ;
+				cout<< "\tEnter The Email Address of Bank Staff Member :"; cin>>email ;
+				
+				cout<<"\nSuccessfully Registered! : \n";
+				cout<<"***************************************************************"<<endl ;
+				cout<< "\n\t Name \t\t:" <<name;
+				cout<< "\n\t Amount Number\t:"<<id_1;
+				cout<< "\n\t Phone Number \t:"<<phone;
+				cout<< "\n\t Email \t\t:"<<email;
+				cout<<"\n*************************************************************"<<endl ;
+			}
+			else 
+			{
+				cout<<"\nInvalid Input!\n\n";
+				goto rep;
+			}
 			
-				int withdraw,after_bal2;
-				cout << endl  <<  "----------------------------------------------------------------------------------------------------------------------" << endl;
-				cout << "   Enter Whithdraw Amaout : ";
-				cin >> withdraw;
-				cout <<  "----------------------------------------------------------------------------------------------------------------------"<<endl;
-				cout << endl << endl << endl;
-				cout << "\t\t\tYour Actual Amount Is : 200000" << endl << endl;
-				cout << "\t\t\tCongrates Your Amount Has Withdraw Successfully." << endl << endl;
-				after_bal2=200000-withdraw;
-				cout << "\t\t\tYour Account Balance Is : " << after_bal2 << endl << endl;
-				break;
-			
-			default:
-				cout << "Invalid choice....";
-			break;				
 		}
-		
-		
-	}
-		
-	else{
+        void deposit(float amount) {
+            balance += amount;
+            cout << amount << " INR deposited successfully!" << endl;
+        }
+        void withdraw(float amount) {
+            if (amount > balance) {
+                cout << "Insufficient balance. Withdrawal failed." << endl;
+            } else {
+                balance -= amount;
+                cout << amount << " INR withdrawn successfully!" << endl;
+            }
+        }
+        void display() {
+            cout << "Your current balance is " << balance <<"INR"<<  endl;
+        }
+};
+
+int main() {
+    Bank customer;
+    int choice;
+    float amount;
+
+	customer.Banner();
+	customer.Staff();
 	
-		cout << "Invalid Choice";
-	}
-	return 0;
+    while (true) {
+        cout << "\n\nPlease select an option:" << endl;
+        cout << "1. Deposit" << endl;
+        cout << "2. Withdraw" << endl;
+        cout << "3. Check Balance" << endl;
+        cout << "4. Exit" << endl;
+        cout << "\n Option : ";
+        cin >> choice;
+
+        switch(choice) {
+            case 1:
+                cout << "\nEnter deposit amount in INR: ";
+                cin >> amount;
+                customer.deposit(amount);
+                break;
+                
+            case 2:
+                cout << "\nEnter withdrawal amount ";
+                cin >> amount;
+                customer.withdraw(amount);
+                break;
+                
+            case 3:
+                customer.display();
+                break;
+                
+            case 4:
+                cout << "\nThank you for using the Bank System!" << endl;
+                exit(0);
+                
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+        }
+    }
+return 0;
 }
